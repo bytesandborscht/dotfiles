ae6d6253411170906144a13e398e6c77029d8c32
bytesandborscht <contact@bytesandborscht.com>
2025-09-21 23:34:44 +0200

    added ~/.ssh/config

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ if [[ ! -d ~/.config/i3 ]]; then
 	mkdir -p ~/.config/i3
 fi
 
+if [[ ! -d ~/.ssh ]]; then
+	mkdir -p ~/.ssh
+fi
+
 echo "[*] Linking ~/.config/nvim/init.vim .."
 ln -sf $(pwd)/.config/nvim/init.vim ~/.config/nvim/init.vim
 echo "[*] Linking ~/.config/i3/config .."
@@ -22,3 +26,5 @@ echo "[*] Linking ~/.alacritty.toml"
 ln -sf $(pwd)/.alacritty.toml ~/.alacritty.toml
 echo "[*] Linking ~/.gitconfig"
 ln -sf $(pwd)/.gitconfig ~/.gitconfig
+echo "[*] Linking ~/.ssh/config"
+ln -sf $(pwd)/.ssh/config ~/.ssh/config
